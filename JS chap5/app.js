cf2437a2b18f66c276117d6fabe808fea282ec4c
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-02-02 18:04:12 +0500

    Add files via upload

---
+++ JS chap5/app.js
@@ -0,0 +1,140 @@
+        // Chap#5
+// Question#1
+// var num1= 3
+// var num2= 5
+// var sum= num1+num2
+// document.write("<p>Sum of "+num1+" and "+num2+" is "+sum+"</p>")
+
+// Question#2
+// var num1= 3
+// var num2= 5
+// var sum= num1-num2
+// document.write("<p>SUBTRACTION of "+num1+" and "+num2+" is "+sum+"</p>")
+
+// var num1= 3
+// var num2= 5
+// var sum= num1*num2
+// document.write("<p>Multiplication of "+num1+" and "+num2+" is "+sum+"</p>")
+
+// var num1= 3
+// var num2= 5
+// var sum= num1/num2
+// document.write("<p>Division of "+num1+" and "+num2+" is "+sum+"</p>")
+
+// Question#3
+// var value= 
+// document.write("<p>Value after variable declaration is "+value+"</p>")
+// var initialNumber=5
+// document.write("<p>Initial value: "+initialNumber+"</p>")
+// var incrementNumber= initialNumber+1
+// document.write("<p>Value after increment is: "+incrementNumber+"</p>")
+// var addition=incrementNumber+7
+// document.write("<p>Value after addition is: "+addition+"</p>")
+// var decrement=addition-1
+// document.write("<p>Value after decrement is :"+decrement+"</p>")
+// var output=decrement%3
+// document.write("<p>The remainder is : "+output+"<p/>")
+
+// Question#4
+// var costOfTicket=600
+// var noOfTicket=5
+// var total=costOfTicket*noOfTicket
+// document.write("<p><b>Total cost to buy "+noOfTicket+" ticket to a movie is "+total+"PKR</b></p>")
+
+// Question#5
+// var num1=4
+// document.write("<p>4 x 1 = "+num1+"</p>")
+// var num=num1*2
+// document.write("<p>4 x 2 = "+num+"</p>")
+// var num=num1*3
+// document.write("<p>4 x 3 = "+num+"</p>")
+// var num=num1*4
+// document.write("<p>4 x 4 = "+num+"</p>")
+// var num=num1*5
+// document.write("<p>4 x 5 = "+num+"</p>")
+// var num=num1*6
+// document.write("<p>4 x 6 = "+num+"</p>")
+// var num=num1*7
+// document.write("<p>4 x 7 = "+num+"</p>")
+// var num=num1*8
+// document.write("<p>4 x 8 = "+num+"</p>")
+// var num=num1*9
+// document.write("<p>4 x 9 = "+num+"</p>")
+// var num=num1*10
+// document.write("<p>4 x 10 = "+num+"</p>")
+
+
+// Question#6
+// var celeius=25
+// fahrenheit=(celeius*(9/5))+32
+// document.write("<p>25C is "+fahrenheit+"F</p> ")
+// f=70
+// c=(f-32)*(5/9)
+// document.write("<p>70F is "+c+"C</p> ")
+
+// Question#7
+
+// var price1=650
+// var item1=3
+// totalOf1Product=price1*item1
+// var price2=100
+// var item2=7
+// shipping=100
+// totalOf2Product=price2*item2
+// totalBill=totalOf1Product+totalOf2Product+shipping
+// document.write("<p>Price of item 1 is "+price1+"</p>")
+// document.write("<p>Quantity of item 1 is "+item1+"</p>")
+// document.write("<p>Price of item 2 is "+price2+"</p>")
+// document.write("<p>Quantity of item 2 is "+item2+"</p>")
+// document.write("<p>Shipping Charges "+shipping+"</p>")
+// document.write("<p>Total Bill "+totalBill+"</p>")
+
+// Question#8
+// var totalMarks=1100
+// var marksObtain=980
+// percentage=(marksObtain*100)/totalMarks
+// document.write("<p>Total Marks: "+totalMarks+"</p>")
+// document.write("<p>Marks Obtain: "+marksObtain+"</p>")
+// document.write("<p>Perxentage: "+percentage+"</p>")
+
+//Question#9
+// var usd=10
+// var riyal=25
+// var usdInPk=usd*104.80
+// var riyalInPk=riyal*28
+// var total=usdInPk+riyalInPk
+// document.write("<p>Total Currency In PKR Is: "+total+"</p>")
+
+//Question#10
+// var num=10
+// var cal=(((num+5)*10)/2)
+// console.log(cal)
+
+//Question#11
+// var currentYear=2023
+// var birthYear=2003
+// var age=currentYear-birthYear
+// document.write("<p>Current Year: "+currentYear+"</p>")
+// document.write("<p>Birth Year: "+birthYear+"</p>")
+// document.write("<p>Your Age: "+age+"</p>")
+
+//Question#12
+// var radius=20
+// var circumference=2*3.14*radius
+// var area=3.14*(radius**2)
+// document.write("<p>Radius of circle: "+radius+"</p>")
+// document.write("<p>Circumference of circle: "+circumference+"</p>")
+// document.write("<p>Area of circle: "+area+"</p>")
+
+//Question#13
+
+// var age = 15;
+// var maxAge = 65;
+// var numPerDay = 3;
+// var totalNeeded = (numPerDay * 365) * (maxAge - age);
+// var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+// document.write("<p>"+message+"</p>");
+
+
+
+
